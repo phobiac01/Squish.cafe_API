30d96c65913f89c4f9094487082ebe03a92bdf02
phobiac01 <phobiac01@gmail.com>
2020-08-02 13:23:48 -0700

    gitignore works now

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const schemas = require('./schemas.js');
 const express = require('express');
 const app = express();
 const port = 8080;
@@ -7,3 +8,15 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/containerStop', (req, res) => process.exit(0));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
+
+
+// Open up remote server: copy oevr and clean up any useable code
+// Mock up a psudocode schema 
+// (maybe use one of the fancy new database mockup services)
+
+// http://localhost:4200
+// https://squish.cafe/AAAAA
+// https://squish.cafe/api/test
+// <<replace with trello link>>
+// <<replace with GitHub link>>
